20ba83f84be395714a8745fcd39a62690fc1344b
Jaroslaw Charchula <charchula@gmail.com>
2022-06-20 20:32:03 +0200

    finish Cryptocurrencies component. add dynamic search bar.

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
@@ -10,12 +10,26 @@ const Cryptocurrencies = ( { simplified } ) => {
   const count = simplified ? 10 : 100;
   const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
   const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    setCryptos(cryptosList?.data?.coins);
+
+    const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm));
+
+    setCryptos(filteredData);
+  }, [cryptosList, searchTerm])
 
   if(isFetching) return 'Loading...';
   
 
   return (
     <>
+      {!simplified && (
+        <div className='search-crypto'>
+          <Input placeholder='Search Cryptocurrency' onChange={(e) =>(setSearchTerm(e.target.value))}/>
+        </div>
+      )}
       <Row gutter={[32, 32]} className='crypto-card-container'>
         {cryptos?.map((currency) => (
           <Col 
